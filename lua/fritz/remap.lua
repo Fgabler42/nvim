f3ae1fbd4953bbdfcc40e908af26b60362302c92
Fritz Gäbler <fritzlmgaebler@gmail.com>
2024-04-16 14:13:29 +0200

    add suspending to leader s

--- lua/fritz/remap.lua
+++ lua/fritz/remap.lua
@@ -3,24 +3,22 @@ vim.g.mapleader = " "
 -----------ALL-LEADER-----------------------
 vim.keymap.set("n", "<leader>pv", function() vim.cmd("Ex") end)
 vim.keymap.set("n", "<leader>h", function() vim.cmd("Std") end)
-vim.keymap.set("n", "<leader>vwm", function() require("vim-with-me").StartVimWithMe() end)
+--vim.keymap.set("n", "<leader>vwm", function() require("vim-with-me").StartVimWithMe() end)
+--vim.keymap.set("n", "<leader>svwm", function() require("vim-with-me").StopVimWithMe() end)
 vim.keymap.set("n", "<leader>l", ":")
-vim.keymap.set("n", "<leader>svwm", function() require("vim-with-me").StopVimWithMe() end)
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
 vim.keymap.set('n', '<leader>k', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Change word under cursor' })
 vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.api.nvim_set_keymap('n', '<leader>a', ':suspend<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>s", function() vim.cmd("suspend") end)
 
 
 vim.keymap.set("n", "Q", "<nop>")
